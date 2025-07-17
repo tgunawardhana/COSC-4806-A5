bafa47655b022cc07e06a6809ace3531d74b87b4
tgunawardhana <tgunawardhana@algomau.ca>
2025-07-17 16:38:54 +0000

    Add functions to get report data

---
+++ app/models/Report.php
@@ -0,0 +1,34 @@
+<?php
+
+class Report {
+
+  public function __construct() {
+
+  }
+
+  public function get_all_reminders() {
+    $dbh = db_connect();
+    $statement = $dbh->prepare("select * from reminders;");
+    $statement->execute();
+    $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  public function get_max_reminders() {
+      $dbh = db_connect();
+      $statement = $dbh->prepare("select user_name from users where id = (select user_id from reminders group by user_id order by count(*) desc limit 1);");
+      $statement->execute();
+      $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+      return $rows;
+  }
+
+  public function get_all_logins($username){
+    $dbh = db_connect();
+    $statement = $dbh->prepare("select * from login_attempts where username = :username;");
+    $statement->bindParam(':username', $username);
+    $statement->execute();
+    $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+
+
+}
